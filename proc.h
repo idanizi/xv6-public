341983ba5ece922e3656d1a736021163b5ab5af5
Idan Izicovich <idan.izicovich@intel.com>
2016-08-27 02:48:55 +0300

    editing exit system call to have proc->status support - staged

--- proc.h
+++ proc.h
@@ -70,7 +70,7 @@ struct proc {
   struct file *ofile[NOFILE];  // Open files
   struct inode *cwd;           // Current directory
   char name[16];               // Process name (debugging)
-  int errno;                   // CHANGED process exit status field #tesk1 #wormUp
+  int status;                   // CHANGED: process exit status field #tesk1 #wormUp
 };
 
 // Process memory is laid out contiguously, low addresses first:
