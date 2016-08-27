7be1e2b1f5d4872c05ea114e028fe589fa1ac33e
Idan Izicovich <idan.izicovich@intel.com>
2016-08-27 03:20:18 +0300

    void cat - argument 'status' of function 'exit' call.

--- cat.c
+++ cat.c
@@ -4,36 +4,32 @@
 
 char buf[512];
 
-void
-cat(int fd)
-{
-  int n;
+void cat(int fd) {
+    int n;
 
-  while((n = read(fd, buf, sizeof(buf))) > 0)
-    write(1, buf, n);
-  if(n < 0){
-    printf(1, "cat: read error\n");
-    exit();
-  }
+    while ((n = read(fd, buf, sizeof(buf))) > 0)
+        write(1, buf, n);
+    if (n < 0) {
+        printf(1, "cat: read error\n");
+        exit(0); // changed
+    }
 }
 
-int
-main(int argc, char *argv[])
-{
-  int fd, i;
+int main(int argc, char *argv[]) {
+    int fd, i;
 
-  if(argc <= 1){
-    cat(0);
-    exit();
-  }
-
-  for(i = 1; i < argc; i++){
-    if((fd = open(argv[i], 0)) < 0){
-      printf(1, "cat: cannot open %s\n", argv[i]);
-      exit();
+    if (argc <= 1) {
+        cat(0);
+        exit(0); // changed
     }
-    cat(fd);
-    close(fd);
-  }
-  exit();
+
+    for (i = 1; i < argc; i++) {
+        if ((fd = open(argv[i], 0)) < 0) {
+            printf(1, "cat: cannot open %s\n", argv[i]);
+            exit(0); // changed
+        }
+        cat(fd);
+        close(fd);
+    }
+    exit(0); // changed
 }
