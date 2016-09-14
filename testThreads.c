7911437334a545674873ccaf383c232a50134489
Idan Izicovich <idanizi@gmail.com>
2016-09-14 23:01:42 +0300

    task1.2: user space test for threads - fail -  unexpected trap 14 from cpu 1 eip a (cr2=0x2fe8f485)  cpu1: panic: trap   0 0 0 0 0 0 0 0 0 0

--- testThreads.c
+++ testThreads.c
@@ -4,14 +4,28 @@
 #include "stat.h"
 #include "user.h"
 
+void run() {
+    int id = kthread_id();
+    int pid = getpid();
+    int time = uptime();
+    printf(1, "thread id: %d\n", id);
+    printf(1, "process pid: %d\n", pid);
+    while(uptime() - time < 1000);
+    printf(1, "hey\n");
+    kthread_exit();
+}
+
 int main(int argc, char *argv[]){
-    // TODO: write a user program to test all the system calls stated above
-    /*
-     * int kthread_create( void*(*start_func)(), void* stack, int stack_size);
-     * int kthread_id();
-     * void kthread_exit();
-     * int kthread_join(int thread_id);
-     */
-    // todo: malloc for *stack at the user space program, pass the pointer by sysprpc.c to the system call in the kernel
+    void *stack = (void *) malloc(4000);
+    void *(*start_func)();
+    start_func = (void *) &run;
+    int pid = getpid();
+    printf(1, "pid of current process: %d\n", pid);
+    printf(1, "creating thread\n");
+    int tid = kthread_create(start_func, stack, 4000);
+    printf(1, "join thread\n");
+    int rest = kthread_join(tid);
+    printf(1, "after join");
+    printf(1, "result: %d, tid: %d\n", rest, tid);
     exit();
 }
