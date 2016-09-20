3092af4e0e8976ee5e8d8c36ac1d767dcc494160
Idan Izicovich <idanizi@gmail.com>
2016-09-20 15:15:42 +0300

    task2.2 #compile & running

--- fssp.c
+++ fssp.c
@@ -6,35 +6,67 @@
 #include "user.h"
 
 // CONSTANTS
-#define NSTATE 15
 #define USTACK 4000
 
-// red
-#define ACTIVE 1
-// white
-#define QUIESCENT 0
-// blue
-#define FIRING 15
-
-//#define PURPLE 2
-//#define AQUA 3
-//#define GRASS 4
-//#define BROWN 5
-//#define DARK_BLUE 6
-//#define GREEN 7
-//#define YELLOW 8
-//#define NAVY 9
-//#define PINK 10
-//#define VIOLATE 11
-//#define VIOLATE 12
-//#define VIOLATE 13
-//#define VIOLATE 14
-
-
-
-
+#define NSTATE 6
+#define Q 'Q'
+#define Z 'Z'
+#define P 'P'
+#define M 'M'
+#define R 'R'
+#define F 'F'
 
 // structs
+
+struct semaphore {
+    int s1;
+    int s2;
+    int value;
+    int wake;
+};
+
+void semaphore_init(struct semaphore *sm, int value) {
+    sm->s1 = kthread_mutex_alloc();
+    sm->s2 = kthread_mutex_alloc();
+    sm->value = value;
+    sm->wake = 0;
+}
+
+int semaphore_delete(struct semaphore *sm) {
+    if (kthread_mutex_dealloc(sm->s1) < 0)
+        return -1;
+    if (kthread_mutex_dealloc(sm->s2) < 0)
+        return -1;
+    return 0;
+}
+
+void semaphore_down(struct semaphore *sm) {
+    kthread_mutex_lock(sm->s1);
+    sm->value--;
+    if (sm->value < 0) {
+        kthread_mutex_unlock(sm->s1);
+        kthread_mutex_lock(sm->s2);
+        kthread_mutex_lock(sm->s1);
+        sm->wake--;
+        if (sm->wake > 0) {
+            kthread_mutex_unlock(sm->s2);
+        }
+    }
+    kthread_mutex_unlock(sm->s1);
+}
+
+void semaphore_up(struct semaphore *sm) {
+    kthread_mutex_lock(sm->s1);
+    sm->value++;
+    if (sm->value <= 0) {
+        sm->wake++;
+        if (sm->wake == 1) {
+            kthread_mutex_unlock(sm->s2);
+        }
+    }
+    kthread_mutex_unlock(sm->s1);
+}
+
 struct soldier {
     struct soldier *leftNeighbor;
     struct soldier *rightNeighbor;
@@ -52,27 +84,76 @@ struct soldier {
 int n;
 struct soldier *squad;
 int mutex;
-int cycle;
-int visitors;
+struct semaphore start;
+struct semaphore end;
+int start_cycle;
+int start_full;
+int end_cycle;
+int end_full;
 
-void done(){
-    kthread_mutex_lock(mutex);
-    cycle++;
-    kthread_mutex_dealloc(mutex);
+void print(struct soldier *squad) {
+    struct soldier *s = 0;
+    for (s = squad; s < &squad[n]; s++) {
+        printf(1, "%X ", s->state);
+    }
+    printf(1, "\n");
 }
 
-int areAllDone(){
+void start_round() {
+    kthread_mutex_lock(mutex);
+    start_cycle++;
+    kthread_mutex_unlock(mutex);
+    semaphore_down(&start);
+}
+
+void end_round() {
+    kthread_mutex_lock(mutex);
+    end_cycle++;
+    kthread_mutex_unlock(mutex);
+    semaphore_down(&end);
+}
+
+int areEverybodyGotToBarrier() {
     int ans = 0;
     kthread_mutex_lock(mutex);
-    visitors++;
-    ans = (cycle == n);
+    ans = (start_cycle == n);
     if(ans){
-        cycle = 0;
-        print(squad);
+        start_full = 1;
+        start_cycle = 0;
+        kthread_mutex_unlock(mutex);
+        return ans;
     }
-    kthread_mutex_dealloc(mutex);
+    ans = (end_cycle == n);
+    if (ans) {
+        end_full = 1;
+        end_cycle = 0;
+        kthread_mutex_unlock(mutex);
+        return ans;
+    }
+    kthread_mutex_unlock(mutex);
     return ans;
 }
+
+//int areAllDone(){
+//    int ans = 0;
+//    kthread_mutex_lock(mutex);
+//    ans = (cycle == n);
+//    if(ans){
+//        cycle = 0;
+//        print(squad);
+//    }
+//    kthread_mutex_dealloc(mutex);
+//    return ans;
+//}
+//
+//int barrio() {
+//    int ans = 0;
+//    kthread_mutex_lock(mutex);
+//    visitors++;
+//
+//    kthread_mutex_unlock(mutex);
+//    return ans;
+//}
 
 struct soldier *getSoldierByTid(int tid) {
     struct soldier *s;
@@ -94,24 +175,26 @@ void regularSoldierTrans() {// todo implement
         kthread_exit();
     }
 
-    while (s->state != FIRING) {
+    while (s->state != F) {
+
+        // start - blocking
+        start_round();
+
         switch (s->state) {
-            case QUIESCENT:
-                if (s->leftNeighbor == QUIESCENT && s->rightNeighbor == QUIESCENT)
-                    s->futureState = QUIESCENT;
+            case F:
+                if (s->leftNeighbor->state == Q && s->rightNeighbor->state == Q)
+                    s->futureState = Q;
                 else
                 break;
             default:
-                while (s->futureState != FIRING) {
+                while (s->futureState != F) {
                     s->futureState = (s->state + 1) % NSTATE;
                 }
                 break;
         }
         s->state = s->futureState;
-        done();
-        while (!areAllDone()){
-            sleep(10);
-        };
+        // end - blocking
+        end_round();
     }
 
     kthread_exit();
@@ -127,7 +210,7 @@ void generalTrans() {// todo implement
         kthread_exit();
     }
 
-    while (s->state != FIRING) { // todo
+    while (s->state != F) { // todo
     }
 
     kthread_exit();
@@ -143,26 +226,34 @@ void firstSoldierTrans() {// todo implement
         kthread_exit();
     }
 
-    while (s->state != FIRING) { // todo
+    while (s->state != F) { // todo
     }
 
     kthread_exit();
 }
 
-void print(struct soldier *squad) {
+int areEverybodyFiring(struct soldier *squad) {
     struct soldier *s = 0;
+    kthread_mutex_lock(mutex);
     for (s = squad; s < &squad[n]; s++) {
-        printf(1, "%X ", s->state);
+        if(s->state != F) {
+            kthread_mutex_unlock(mutex);
+            return 0;
+        }
     }
-    printf(1, "\n");
+    kthread_mutex_unlock(mutex);
+    return 1;
 }
 
 int main(int argc, char **argv) {
 
     n = 0;
     squad = 0;
-    cycle = 0;
-    visitors = 0;
+    start_cycle = 0;
+    start_full = 0;
+    end_cycle = 0;
+    end_full = 0;
+    int finish = 0;
 
     mutex = kthread_mutex_alloc();
 
@@ -174,6 +265,8 @@ int main(int argc, char **argv) {
     // initiating by parameters
     n = atoi(argv[1]);
     squad = (struct soldier *) malloc(n * sizeof(struct soldier));
+    semaphore_init(&start, n);
+    semaphore_init(&end, 0);
 
     // init soldiers
     struct soldier *s = 0;
@@ -183,7 +276,7 @@ int main(int argc, char **argv) {
     for (s = squad; s < &squad[n]; s++, i++) {
 
         // initiating to zeros
-        s->state = QUIESCENT;
+        s->state = Q;
         s->id = i;
         s->leftNeighbor = 0;
         s->rightNeighbor = 0;
@@ -201,24 +294,55 @@ int main(int argc, char **argv) {
             s->rightNeighbor = squad + (i + 1);
         else {
             s->func = (void *) &generalTrans;
-            s->state = ACTIVE;
+            s->state = P;
         }
         if (!general && !first) // regular soldier
             s->func = (void *) &regularSoldierTrans;
 
         // memo allocation
-        stack = (void *) malloc(USTACK);
+        s->stack = (void *) malloc(USTACK);
 
         // creating thread for this soldier
         s->tid = kthread_create(s->func, s->stack, USTACK);
     }
 
+    // print initial state
+    print(squad);
+
+    // main process looking after threads and release them from barrier all together
+    while (!finish) {
+        while (!areEverybodyGotToBarrier()) {
+            sleep(10);
+        }
+
+        // print current state
+        print(squad);
+
+        if (start_full) {
+            start_full = 0;
+            for (i = 0; i < n; i++) {
+                semaphore_up(&start);
+            }
+        } else if (end_full) {
+            end_full = 0;
+            for (i = 0; i < n; i++) {
+                semaphore_up(&end);
+            }
+            finish = areEverybodyFiring(squad);
+        } else {
+            printf(1, "Error: got to barrier without full\n");
+        }
+    }
 
 //    print(squad);
 
     // wait for all to finish
     for (s = squad; s < &squad[n]; s++) {
         kthread_join(s->tid);
+    }
+
+    if (semaphore_delete(&start) < 0 || semaphore_delete(&end) < 0) {
+        printf(1, "Error: semaphore delete\n");
     }
 
     if (kthread_mutex_dealloc(mutex) < 0)
